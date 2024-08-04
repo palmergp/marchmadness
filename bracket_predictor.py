625ceb6b4f0cf5d0a7ce468d2235c1419f52e2e9
palmergp <gp.palmer@verizon.net>
2024-08-03 22:21:16 -0400

    Modified Matchup predictor to use a predict function. Got bracket predictor started

--- bracket_predictor.py
+++ bracket_predictor.py
@@ -1,8 +1,37 @@
 """Given a year, uses a model to predict the bracket and calculate point total"""
+from matchup_predictor import MatchupPredictor
+from scraping.get_tournament_data import get_tournament_data
 
 
 class BracketPredictor:
     """Goes through a tournament and predicts all matchups"""
 
-    def __init__(self, model):
-        self.model =
+    def __init__(self, model, year):
+        self.predictor = MatchupPredictor(model)
+        self.bracket = get_tournament_data(year)
+        self.year = year
+
+    def main(self):
+        # This needs to be able to pull in the round one matchups and figure out matchups from there
+        # Cannot use anything after round 1 since missed picks affect future matchups
+        points = 0
+        for game in self.bracket:
+            result = self.predictor.predict(
+                first_team=game["team1"],
+                first_seed=game["team1_seed"],
+                second_team=game["team2"],
+                second_seed=game["team2_seed"],
+                round=game["round"],
+                year=self.year,
+            )
+            if tourney_over:
+                # If the tourney is over, check the result to see if the prediction was right
+                pass
+
+if __name__ == '__main__':
+    path = "models/models24/v24_3_1/"
+    model = "Linear_SVC_v24_3_1.package"
+    year = 2024
+    tourney_over = True
+    bp = BracketPredictor(path+model,2024)
+    bp.main(tourney_over)
--- matchup_predictor.py
+++ matchup_predictor.py
@@ -43,27 +43,136 @@ class MatchupPredictor:
                 self.features = [x.replace("Team1", "favorite_") for x in self.features]
                 self.features = [x.replace("Team2", "underdog_") for x in self.features]
 
-    def predict(self, team1, team1_seed, team2, team2_seed, round):
+    def predict(self, first_team, first_seed, second_team, second_seed, round, year, show_plots=False):
+        """Predicts a matchup
+        Input:
+            - first_team: (str) the name of the first team in the matchup
+            - first_seed: (int) the seed of the first team in the matchup
+            - second_team: (str) the name of the second team in the matchup
+            - second_seed: (int) the seed of the second team in the matchup
+            - round: (int) the round the matchup takes palce in
+            - year: (int) the year in which the matchup takes place
+            - show_plots: (bool) indicates whether plots should be created or not. Defaults to false
+        Output:
+         - result: (int) Indicates if team one or team two won. Output will either be 1 or 2. A -1 is
+                    returned if an error occurs
+        """
         try:
+            # Try to load data
             print("Fetching team stats...")
-            first_data_true = self.data.loc[reformat_name(team1)]
-            first_roster_true = get_roster_stats([reformat_name(team1)], year).loc[reformat_name(first)]
-            first_schedule_true = get_schedule_stats([reformat_name(first)], year).loc[reformat_name(first)]
-            print("Successfully loaded {} stats for {}".format(year, first))
-            not_loaded = False
+            first_data = self.data.loc[reformat_name(first_team)]
+            first_roster = get_roster_stats([reformat_name(first_team)], year).loc[reformat_name(first_team)]
+            first_schedule = get_schedule_stats([reformat_name(first_team)], year).loc[reformat_name(first_team)]
+            print("Successfully loaded {} stats for {}".format(year, first_team))
+            second_data = self.data.loc[reformat_name(second_team)]
+            second_roster = get_roster_stats([reformat_name(second_team)], year).loc[reformat_name(second_team)]
+            second_schedule = get_schedule_stats([reformat_name(second_team)], year).loc[reformat_name(second_team)]
+            print("Successfully loaded {} stats for {}".format(year, second_team))
         except KeyError as e:
-            print("Unable to load {}. Make sure it is spelled like it is in the following list:".format(first))
-            print(all_teams)
+            print("Unable to load {}. Make sure it is spelled like it is in the following list:".format(first_team))
+            print(self.data)
             print(e)
-        result = self.model.predict(data)
+            result = -1
+            return result
+        # Make sure team 1 is the lower seed (better team)
+        if first_seed <= second_seed:
+            team1 = first_team
+            team2 = second_team
+            team1_seed = first_seed
+            team1_roster = first_roster
+            team1_data = first_data
+            team1_schedule = first_schedule
+            team2_seed = second_seed
+            team2_roster = second_roster
+            team2_data = second_data
+            team2_schedule = second_schedule
+        else:
+            team2=first_team
+            team1=second_team
+            team2_seed = first_seed
+            team2_roster = first_roster
+            team2_data = first_data
+            team2_schedule = first_schedule
+            team1_seed = second_seed
+            team1_roster = second_roster
+            team1_data = second_data
+            team1_schedule = second_schedule
+        print(
+            "{} {} is being used as the underdog and {} {}  is being used as the favorite".format(team2_seed, team2,
+                                                                                                  team1_seed,
+                                                                                                  team1))
+        # Get player and schedule stats
+        for rost_stat in team1_roster.index:
+            team1_data[rost_stat] = team1_roster[rost_stat]
+        for sch_stat in team1_schedule.index:
+            team1_data[sch_stat] = team1_schedule[sch_stat]
+        for rost_stat in team2_roster.index:
+            team2_data[rost_stat] = team2_roster[rost_stat]
+        for sch_stat in team2_schedule.index:
+            team2_data[sch_stat] = team2_schedule[sch_stat]
+        # Get Bracket features
+        predict_data = []
+        for feat in self.features:
+            if feat == "Round":
+                predict_data.append(round)
+            elif feat == "favorite_seed" or feat == "team1_seed":
+                predict_data.append(team1_seed)
+            elif feat == "underdog_seed" or feat == "team2_seed":
+                predict_data.append(team2_seed)
+            elif feat == "SeedDiff":
+                predict_data.append(abs(team1_seed - team2_seed))
+            else:
+                if "favorite" in feat:
+                    predict_data.append(team1_data[feat.replace("favorite_", "")])
+                else:
+                    predict_data.append(team2_data[feat.replace("underdog_", "")])
+        # If it was a scaled model, scale the features
+        if self.scaler:
+            predict_data = self.scaler.transform([predict_data])[0]
+        # Make prediction
+        print("Making prediction")
+        winner_probs = self.model.predict_proba([predict_data])[0]
+        print("\n-------------------------------------")
+        if winner_probs[0] > winner_probs[1]:
+            winner_name = team1
+            print("The winner will be {}".format(team1))
+            print(
+                "Probability split:\n\t{}: {}\n\t{}: {}".format(team1, winner_probs[0], team2, winner_probs[1]))
+        elif winner_probs[0] <= winner_probs[1]:
+            winner_name = team2
+            print("The winner will be {}".format(team2))
+            print(
+                "Probability split:\n\t{}: {}\n\t{}: {}".format(team1, winner_probs[0], team2, winner_probs[1]))
+        else:
+            winner_name = ""
+            print("Error: Returned value was unexpected!!")
+
+        # Check if winner was input as team one or two
+        if winner_name == first_team:
+            result = 1
+        elif winner_name == second_team:
+            result = 2
+        else:
+            result = -1
+        # From classifier in predict
+        # Only show if upset
+        if self.explainer is not None and winner_probs[0] <= winner_probs[1] and show_plots:
+            df = pd.DataFrame([predict_data], columns=self.features)
+            df = df.astype("float64")  # final is the df of scaled features
+            shap_values = self.explainer(df)
+            plt.figure()  # plt is matplotlib
+            f = shap.plots.waterfall(shap_values[0], show=False)
+            f.set_title(f"Left ({team1_seed}) {team1}, Right ({team2_seed}) {team2}".title())
+            plt.tight_layout()
+            plt.show()
+        print("-------------------------------------\n")
+        print("Preparing for next prediction...\n")
+
         return result
 
     def set_year(self, year):
         """Loads the data for a given year to be used for predictions"""
-        self.data = {
-            "team": get_team_stats(year),
-            "roster": get_roster_stats()
-        }
+        self.data = get_team_stats(year)
 
     def main(self):
         year = int(input("Enter the tournament year: "))
@@ -74,7 +183,8 @@ class MatchupPredictor:
             second_team = input("Team2 Name: ")
             second_seed = int(input("Team2 Seed: "))
             round = int(input("Round: "))
-            self.predict(first_team, first_seed, second_team, second_seed, round)
+            result = self.predict(first_team, first_seed, second_team, second_seed, round, year)
+            print(result)
 
 
     def main_old(self, year):
@@ -199,9 +309,10 @@ class MatchupPredictor:
             print("-------------------------------------\n")
             print("Preparing for next prediction...\n")
 
+
 if __name__ == '__main__':
     path = "models/models24/v24_3_1/"
     model = "Linear_SVC_v24_3_1.package"
     mp = MatchupPredictor(path+model, features=path+"featurenames.pickle")
     now = datetime.now()
-    mp.main(now.year)
+    mp.main()
